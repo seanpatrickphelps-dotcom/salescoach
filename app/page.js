3d323b50d2c1a54254ce9eb7f914a72681de610b
seanpatrickphelps-dotcom <seanpatrickphelps@gmail.com>
2026-05-26 20:21:40 -0500

    Create Home component for Sales Coach page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>Sales Coach</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
